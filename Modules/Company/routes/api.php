269932661a689d039407a7578ba64bac40821baf
Eng.Muhamed Khyr Awad <119810917+Mhmdawad2000@users.noreply.github.com>
2025-06-08 02:45:20 +0300

    Revert "Development"

--- Modules/Company/routes/api.php
+++
@@ -1,8 +0,0 @@
-<?php
-
-use Illuminate\Support\Facades\Route;
-use Modules\Company\Http\Controllers\CompanyController;
-
-Route::middleware(['auth:sanctum'])->prefix('v1')->group(function () {
-    Route::apiResource('companies', CompanyController::class)->names('company');
-});
--- Modules/Trip/routes/api.php
+++
@@ -1,8 +0,0 @@
-<?php
-
-use Illuminate\Support\Facades\Route;
-use Modules\Trip\Http\Controllers\TripController;
-
-Route::middleware(['auth:sanctum'])->prefix('v1')->group(function () {
-    Route::apiResource('trips', TripController::class)->names('trip');
-});
