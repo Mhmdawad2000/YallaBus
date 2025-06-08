f7f6b46b3ab173fcf17c388191dcf6c15fa7ee31
MohAwad <muhamedawad2000@gmail.com>
2025-06-08 14:53:42 +0300

    fix 2

--- Modules/Settings/database/migrations/0001_01_01_000004_create_currencies_table.php
+++ Modules/Settings/database/migrations/0001_01_01_000004_create_currencies_table.php
@@ -3,8 +3,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-return new class extends Migration 
-{
+return new class extends Migration {
     public function up()
     {
         Schema::create('currencies', function (Blueprint $table) {
@@ -23,4 +22,4 @@ return new class extends Migration
     {
         Schema::dropIfExists('currencies');
     }
-}
+};
